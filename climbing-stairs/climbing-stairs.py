b11a561e9441b9608f27d959fe4664dfd53b7d8e
Liben Hailu <libenhailu04@gmail.com>
2025-02-06 11:10:09 +0300

    Time: 0 ms, Memory: 17.7 MB - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,15 +1,25 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        def dp(n):
-            if n in memo:
-                return memo[n]
-            if n == 0:
-                return 1
-            if n < 0:
-                return 0
-            ans = dp(n - 1) + dp(n - 2)
-            memo[n] = ans
-            return memo[n]
-        return dp(n)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp =  [0] * n
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
+#         memo = {}
+#         def dp(n):
+#             if n in memo:
+#                 return memo[n]
+#             if n == 0:
+#                 return 1
+#             if n < 0:
+#                 return 0
+#             ans = dp(n - 1) + dp(n - 2)
+#             memo[n] = ans
+#             return memo[n]
+#         return dp(n)
         
