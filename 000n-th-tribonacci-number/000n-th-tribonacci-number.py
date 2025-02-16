eaa212067c0f327e5437aea90b8d9f369e50dcb3
Liben Hailu <libenhailu04@gmail.com>
2025-02-16 11:44:16 +0300

    Time: 0 ms, Memory: 17.8 MB - LeetHub

--- 000n-th-tribonacci-number/000n-th-tribonacci-number.py
+++ 000n-th-tribonacci-number/000n-th-tribonacci-number.py
@@ -1,13 +1,20 @@
 class Solution:
+    @lru_cache(None)
     def tribonacci(self, n: int) -> int:
+        # if n == 0:
+        #     return 0
+        # if n < 3:
+        #     return 1
+        # memo = [0] * (n + 1)
+        # memo[0] = 0
+        # memo[1] = 1
+        # memo[2] = 1
+        # for i in range(3, n + 1):
+        #     memo[i] = memo[i - 3] + memo[i - 2] + memo[i - 1]
+        # return memo[n]
+        
         if n == 0:
             return 0
         if n < 3:
             return 1
-        memo = [0] * (n + 1)
-        memo[0] = 0
-        memo[1] = 1
-        memo[2] = 1
-        for i in range(3, n + 1):
-            memo[i] = memo[i - 3] + memo[i - 2] + memo[i - 1]
-        return memo[n]
+        return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
