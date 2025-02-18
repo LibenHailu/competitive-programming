69b4aa7b1d645ef0771c65426b4934148b302cf1
Liben Hailu <libenhailu04@gmail.com>
2025-02-18 10:06:08 +0300

    Time: 63 ms, Memory: 18 MB - LeetHub

--- paint-house-ii/paint-house-ii.py
+++ paint-house-ii/paint-house-ii.py
@@ -1,10 +1,21 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
-        @cache
-        def dp(row, prev):
-            if row == len(costs):
-                return 0
+        
+        for r in range(len(costs) - 2, -1, -1):
+            for c in range(len(costs[0])):
+                curr = costs[r][c]
+                min_value = float("inf")
+                for c2 in range(len(costs[0])):
+                    if c2 != c:
+                        min_value = min(min_value, costs[r + 1][c2])
+                costs[r][c] = curr + min_value
+        return min(costs[0])
+#         @cache
+#         def dp(row, prev):
+#             if row == len(costs):
+#                 return 0
             
-            return costs[row][prev] +  min(dp(row + 1, i) for i in range(len(costs[0])) if i != prev) 
+#             return costs[row][prev] +  min(dp(row + 1, i) for i in range(len(costs[0])) if i != prev) 
     
-        return min(dp(0, i) for i in range(len(costs[0]))) 
+#         return min(dp(0, i) for i in range(len(costs[0]))) 
+            
