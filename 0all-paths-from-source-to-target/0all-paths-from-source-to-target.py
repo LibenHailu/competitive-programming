e62f4002522aeb64063afd95ed9e5fcb3cc6398a
Liben Hailu <libenhailu04@gmail.com>
2025-02-28 11:12:16 +0300

    Time: 7 ms, Memory: 19.3 MB - LeetHub

--- 0all-paths-from-source-to-target/0all-paths-from-source-to-target.py
+++ 0all-paths-from-source-to-target/0all-paths-from-source-to-target.py
@@ -1,14 +1,27 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         res = []
-        
-        def backtrack(node, curr):
+        queue = collections.deque([(0, [0])])
+        visited = set()
+        while queue:
+            node, path = queue.popleft()
             if node == len(graph) - 1:
-                res.append(curr[:])
-                return 
+                res.append(path)
+                continue 
+            
             for nei in graph[node]:
-                curr.append(nei)
-                backtrack(nei, curr)
-                curr.pop()
-        backtrack(0, [0])
+                # if nei not in visited:
+                queue.append((nei, path + [nei] ))
         return res
+        # res = []
+        # def backtrack(node, curr):
+        #     if node == len(graph) - 1:
+        #         res.append(curr[:])
+        #         return 
+        #     for nei in graph[node]:
+        #         curr.append(nei)
+        #         backtrack(nei, curr)
+        #         curr.pop()
+        # backtrack(0, [0])
+        # return res
+        
